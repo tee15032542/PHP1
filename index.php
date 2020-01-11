0cfbcddc3a9dc06c6cc9df6cd1057535a3328606
Toey <59773481+tee15032542@users.noreply.github.com>
2020-01-12 01:28:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 
-$site = "MarcusCode";
-$year = 2016;
-$pi = 3.14;
-
-echo var_dump($site);
-echo var_dump($year);
-echo var_dump($pi);
-
-echo isset($site), "\n";
-unset($site);
-echo isset($site), "\n";
+echo "0"
 
 ?>
